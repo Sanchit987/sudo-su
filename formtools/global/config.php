819dc5545f0a1b68e062b591b91df3a84aa550c0
Sanchit987 <kherasanchit1@gmail.com>
2019-10-13 10:26:20 +0530

    formtools/global/config.php file

---
+++ formtools/global/config.php
@@ -0,0 +1,15 @@
+<?php
+
+// main program paths - no trailing slashes!
+$g_root_url = "https://lab.gdy.club/~sanchit987/sudo-su/formtools";
+$g_root_dir = "/home/sanchit987/public_html/sudo-su/formtools";
+
+// database settings
+$g_db_hostname = "localhost";
+$g_db_port = "3306";
+$g_db_name = "akmecha_sudo-su";
+$g_db_username = "akmecha";
+$g_db_password = "alok_akay80!";
+$g_table_prefix = "ft_";
+
+?>
